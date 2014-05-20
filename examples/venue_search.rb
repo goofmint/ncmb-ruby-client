c66868202d6afe7097a62b9fc5e0bdacf69a9b68
Atsushi Nakatsugawa <atsushi@moongift.jp>
2014-05-20 17:05:40 +0900

    Add example

---
+++ examples/venue_search.rb
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$:.unshift(File.dirname(__FILE__))
+require 'rubygems'
+require 'ncmb'
+require 'yaml'
+yaml = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'setting.yml'))
+@ncmb = NCMB.init(application_key: yaml['application_key'], 
+                  client_key: yaml['client_key']
+                  )
+json = JSON.parse(open(File.join(File.dirname(__FILE__), 'venues.json'), 'r').read)
+venues_class = @ncmb.data_store 'Venues'
+json['response']['venues'].each do |venue|
+  params = {
+    name: venue['name'],
+    location: {
+      "__type" => "GeoPoint",
+      "latitude" => venue['location']['lat'],
+      "longitude" => venue['location']['lng']
+    }
+  }
+  puts venues_class.post(params).body
+end
+params = {}
+params[:where] = {
+  "point" => {
+    "$within" => {
+      "$box" => [
+                 {
+                   "__type" => "GeoPoint",
+                   "latitude" => 35.690921,
+                   "longitude" => 139.700258
+                 },
+                 {
+                   "__type" => "GeoPoint",
+                   "latitude" => 35.728926,
+                   "longitude" => 139.71038
+                 }
+                ]
+    }
+  }
+}
+params[:where] = {
+  "location" => {
+    "$nearSphere" => {
+      "__type" => "GeoPoint",
+      "longitude" => 139.745433,
+      "latitude" => 35.691152
+    },
+    "$maxDistanceInKilometers" => 10
+  }
+}
+#  
+puts venues_class.get params
+#puts venues_class.get queries
